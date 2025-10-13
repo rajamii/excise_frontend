69c12c8db92a1d046fb8f144dca2f32933f3e9a8
nicsikkim <nicsikkimbitbucket@gmail.com>
2025-10-13 20:00:30 +0530

    officer_incharge_starts

---
+++ src/app/features/licensee/supplyChain/officer-in-charge/officer-in-charge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-officer-in-charge',
+  imports: [],
+  templateUrl: './officer-in-charge.component.html',
+  styleUrl: './officer-in-charge.component.scss'
+})
+export class OfficerInChargeComponent {
+
+}
