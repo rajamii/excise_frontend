59721135c444a7a5b21fb4432523b6d6fad06d61
ITS_A_PURE_SOUL <109498451+PureSouL11212@users.noreply.github.com>
2025-11-08 12:54:30 +0530

    Hologram Manufacturing Registers 1

---
+++ src/app/features/licensee/supplyChain/HoloGram/hologram-manufacturing-register/hologram-manufacturing-register.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hologram-manufacturing-register',
+  imports: [],
+  templateUrl: './hologram-manufacturing-register.component.html',
+  styleUrl: './hologram-manufacturing-register.component.scss'
+})
+export class HologramManufacturingRegisterComponent {
+
+}
