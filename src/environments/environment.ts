3305088fd0a338456b2d5833e9564f3d7d9a6404
ITS_A_PURE_SOUL <sameerkhan4321087@gmail.com>
2026-03-12 14:29:40 +0530

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    //apiBaseUrl: 'http://10.182.154.196:8000',
-    apiBaseUrl: 'http://127.0.0.1:8000',
+    apiBaseUrl: 'http://10.182.154.196:8000',
+    //apiBaseUrl: 'http://127.0.0.1:8000',
     payment: {
         billdeskGatewayUrl: 'https://uat1.billdesk.com/pgidsk/PGIMerchantPayment',
         callbackUrl: 'http://localhost:4200/payment/callback',
